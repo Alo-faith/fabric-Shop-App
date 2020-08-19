c5e4f1df072da42479e6088c789bd222991a80fb
alo-faith <alo.faith@gmail.com>
2020-08-19 22:35:30 +0300

    add styles

--- components/CartList/styles.js
+++ components/CartList/styles.js
@@ -1,5 +1,6 @@
 import styled from "styled-components/native";
-import { Icon } from "native-base";
+import { Icon, Button } from "native-base";
+
 export const TotalPrice = styled.Text`
   color: red;
   font-size: 15px;
@@ -8,4 +9,24 @@ export const TotalPrice = styled.Text`
 `;
 export const TrashIcon = styled(Icon)`
   color: red;
+`;
+
+export const CheckButton = styled(Button)`
+  align-self: stretch;
+  align-items: center;
+  padding: 10px;
+  background-color: black;
+  margin-top: 30px;
+  text-align: center;
+`;
+export const CheckButtonText = styled.Text`
+  /* color: white;
+  font-weight: bold;
+  font-size: 16px;
+  align-items: center;
+  text-align: center; */
+  text-align: center;
+  color: #fcfdff;
+  font-weight: bold;
+  font-size: 18px;
 `;
