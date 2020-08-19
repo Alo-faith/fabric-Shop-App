7b0b285642865df1ea7cd06243e68e940d9d13b2
alo-faith <alo.faith@gmail.com>
2020-08-19 22:39:33 +0300

    add cart

--- components/CartList/index.js
+++ components/CartList/index.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import { observer } from "mobx-react";
 
-import { Content, List, Spinner, Text } from "native-base";
+import { Content, List, Spinner, Text, View } from "native-base";
 
+// Style
+import { CheckButton, CheckButtonText } from "./styles";
 // store
 import itemStore from "../../stores/itemStore";
 import CartItem from "./CartItem";
 import cartStore from "../../stores/cartStore";
 
-const CartList = () => {
+import authStore from "../../stores/authStore";
+const CartList = ({ navigation }) => {
   if (itemStore.loading) return <Text>Loading</Text>;
   const cartList = cartStore.items
     .map((item) => ({
@@ -17,9 +20,21 @@ const CartList = () => {
     }))
     .map((item) => <CartItem item={item} key={item.name} />);
 
+  const handleCheckout = () => {
+    if (authStore.user) cartStore.checkoutCart();
+    else {
+      navigation.navigate("Signin");
+    }
+  };
+
   return (
     <Content>
       <List>{cartList}</List>
+      <CheckButton onPress={handleCheckout}>
+        <CheckButtonText>
+          {authStore.user ? "Checkout" : "Signin to checkout"}
+        </CheckButtonText>
+      </CheckButton>
     </Content>
   );
 };
